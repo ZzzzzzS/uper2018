2550d563d773c04e4e5371c2b657a217ecb44898
ZISHUN的WINDOWS\m1899 <m18996002212@me.com>
2017-11-08 00:52:52 +0800

    添加logo,界面缩放有问题

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,11 +7,12 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->pix.load("logo.jpg");
-    ui->LogoLabel->setPixmap(this->pix.scaled(ui->LogoLabel->size()));
     QObject::connect(ui->AboutButton,SIGNAL(clicked(bool)),this,SLOT(AboutSlot()));
     QObject::connect(ui->BluetoothButton,SIGNAL(clicked(bool)),this,SLOT(ConfigSlot()));
     QObject::connect(ui->RemoteButton,SIGNAL(clicked(bool)),this,SLOT(ControlSlot()));
     QObject::connect(ui->DebugButton,SIGNAL(clicked(bool)),this,SLOT(HelperSlot()));
+    ui->webEngineView->setUrl(QString("http://zzshub.cn/718logo.jpg"));
+    ui->webEngineView->show();
     this->setMouseTracking(true);
 }
 
